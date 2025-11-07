5462d576d361ef54271e674134d2c263c85e61b8
ShanmukhShashankh <vemula.shanmukhshashankh@gmail.com>
2025-11-08 05:22:57 +0530

    Gradle initialization

---
+++ src/main/java/com/anotherinterpreter/lox/Scanner.java
@@ -0,0 +1,75 @@
+package anotherinterpreter.lox;
+
+import static com.anotherinterpreter.lox.TokenType.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Scanner {
+
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+
+    // first character in the lexeme being scanned
+    private int start = 0;
+    // current character being scanned
+    private int current = 0;
+    // what line current is on
+    private int line = 1;
+
+    Scanner(String source) {
+        this.source = source;
+    }
+
+    List<Token> scanTokens() {
+        while (!isAtEnd()) {
+            start = current;
+            scanToken();
+        }
+
+        tokens.add(new Token(EOF, "", null, line));
+        return tokens;
+    }
+
+    private void scanToken() {
+        char c = advance();
+        switch (c) {
+            case '(':
+                addToken(LEFT_PAREN);
+                break;
+            case ')':
+                addToken(RIGHT_PAREN);
+                break;
+            case '{':
+                addToken(LEFT_BRACE);
+                break;
+            case '}':
+                addToken(RIGHT_BRACE);
+                break;
+            case ',':
+                addToken(COMMA);
+                break;
+            case '.':
+                addToken(DOT);
+                break;
+            case '-':
+                addToken(MINUS);
+                break;
+            case '+':
+                addToken(PLUS);
+                break;
+            case ';':
+                addToken(SEMICOLON);
+                break;
+            case '*':
+                addToken(STAR);
+                break;
+        }
+    }
+
+    private boolean isAtEnd() {
+        return current >= source.length();
+    }
+}
